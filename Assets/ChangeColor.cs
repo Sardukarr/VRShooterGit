4b19db3ed1871f9146b3a5f92d42e135a1806f3c
Sardukar <fremen13@gmail.com>
2020-10-24 19:11:10 +0200

    Project Initialisation with GoogleVR package

---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+public void Red()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void Black()
+    {
+        GetComponent<Renderer>().material.color = Color.black;
+    }
+    public void White()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+}
